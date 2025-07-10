d44bc6d906bdf0b210f92557a1723b84b0dcee3e
Nupur-gitt <146472606+Nupur-gitt@users.noreply.github.com>
2025-07-11 05:24:25 +0600

    Segment Prime(l,r)

--- RangePrimeGenerator.cpp
+++ RangePrimeGenerator.cpp
@@ -1,46 +1,52 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define fast() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0); 
+#define ll long long
 #define endl '\n'
 using namespace std;
-const int mxn = 1e6+123;
+const int mxn = 1e5+12;
 bool isPrime[mxn];
 vector<int>v;
 
-void primeGen(int n){
+void sieve(int n){
     memset(isPrime,1,sizeof(isPrime));
-    isPrime[1] = false;
-    for(int i = 2; i<=n ; i++){
+    isPrime[0] = isPrime[1] = false;
+    for(int i = 2; i * i<=n ; i++){
         if(isPrime[i] == true){
-           for(int j = i + i; j<=n; j+=i){
+           for(int j = i * i; j<=n; j += i){
            isPrime[j] = false; 
         }
         }    
     }
-    isPrime[2] = false;
-    for(int i = 3; i<=n; i++){
-        if(isPrime[i] == 1) v.push_back(i);
+    for(int i = 2; i<=n; i++){
+        if(isPrime[i] == true) v.push_back(i);
     }
+}
+void seg_sieve(int l, int r){
+    int mxn2 = r-l+1;
+    bool isSeg[mxn2];
+    memset(isSeg,1,sizeof(isSeg));
+    if(l == 1) isSeg[0] = false;
+    for(auto u : v){
+        if(u * u > r) break;
+        ll s = max(u * u, (l + u - 1) / u * u);
+          for (ll i = s; i <= r; i += u) {
+            if (i != u) isSeg[i - l] = false;
+        }
+    }
+    for (int i = 0; i < mxn2 ; i++) {
+        if (isSeg[i] == true) cout << l + i << endl;
+    }
+
 }
 int main(){
     fast();
-    primeGen(1e6);
-    int n; 
-    while(cin >> n){
-        if(n == 0) break;
-        int diff = -1, p1 = -1, p2 = -1;
-        for(auto u : v){
-            if(u > n) break;
-            int temp1 = u, temp2 = n - u;
-            if(temp2 > 2 && isPrime[temp2] == 1){
-                diff = abs(temp1 - temp2);
-                p1 = temp1;
-                p2 = temp2;
-                
-            }
-        }
-        if(p1 == -1) cout<< "Goldbach's conjecture is wrong." << endl;
-        else cout << n << " = " << p2 << " + " << p1  << endl;
+    sieve(1e5);
+    int t; cin>>t;
+    while(t--){
+        int l,r;   cin>>l>>r;
+        seg_sieve(l,r);
+        cout << endl;
     }
- return 0;
 }
+
